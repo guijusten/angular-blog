a7b1247947a51637b9a0386413b7baa29a41966f
Guilherme Fiorini <gui.justen@gmail.com>
2024-01-27 19:05:59 -0300

    first commit

---
+++ src/app/data/movies.ts
@@ -0,0 +1,39 @@
+export interface Movie {
+    id:String
+    name:String;
+    photoCover:String;
+    description:String;
+  }
+
+
+export const moviesData = [{
+    id:"1",
+    name:"Batman",
+    photoCover:"a",
+    description:"a"
+},
+{
+    id:"2",
+    name:"Mulher Maravilha",
+    photoCover:"a",
+    description:"a"
+},
+{
+    id:"3",
+    name:"Superman",
+    photoCover:"a",
+    description:"a"
+},
+{
+id:"4",
+    name:"Lanterna Verde",
+    photoCover:"a",
+    description:"a"
+},
+{
+    id:"5",
+    name:"Flash",
+    photoCover:"a",
+    description:"a"
+}
+]
